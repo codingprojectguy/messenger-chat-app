079b63ba1ab051acd117af9bfb41506791abb3db
Home PC <vfxprojectguy@gmail.com>
2024-03-23 13:47:10 -0700

    front end commit

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,6 +4,14 @@ const Register = () => {
   return (
     <div>
       <h1>Register</h1>
+
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="username" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 };
