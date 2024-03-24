8858769c61369f940139204a044acb60fc53a5d7
Home PC <vfxprojectguy@gmail.com>
2024-03-24 15:20:27 -0700

    update on login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,46 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="register">
+      <div className="card">
+        <div className="card_header">
+          <h3>Login</h3>
+        </div>
+        <div className="card-body">
+          <form action="">
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Email"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+
+            <div className="form-group">
+              <input type="submit" value={"login"} className="btn" />
+            </div>
+
+            <div className="form-group">
+              <span>
+                <Link to="/messenger/register">Register Your Account</Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
