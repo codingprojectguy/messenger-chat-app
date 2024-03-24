26a7b5c86908e9bb0b5b94b3f6ec2ba8744a96c3
Home PC <vfxprojectguy@gmail.com>
2024-03-24 15:14:00 -0700

    add register page and login page also scss

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,17 +1,77 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="register">
+      <div className="card">
+        <div className="card_header">
+          <h3>Register</h3>
+        </div>
+        <div className="card-body">
+          <form action="">
+            <div className="form-group">
+              <label htmlFor="username">User Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                placeholder="User Name"
+              />
+            </div>
 
-      <form>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" />
-        <button type="submit">Register</button>
-      </form>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Email"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="confirmPassword"
+                placeholder="Confirm Password"
+              />
+            </div>
+
+            <div className="form-group">
+              <div className="file-image">
+                <div className="image"></div>
+                <div className="file">
+                  <label htmlFor="image">Select Image</label>
+                  <input type="file" className="form-control" id="image" />
+                </div>
+              </div>
+            </div>
+
+            <div className="form-group">
+              <input type="submit" value={"register"} className="btn" />
+            </div>
+
+            <div className="form-group">
+              <span>
+                <Link to="/messenger/login">Login Your Account</Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
