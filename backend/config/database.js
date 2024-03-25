84ab947626c0dad863eba7860498ad237150f2a9
Home PC <vfxprojectguy@gmail.com>
2024-03-25 13:50:06 -0700

    build backend database

---
+++ backend/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const databaseConnect = () => {
+  mongoose
+    .connect(process.env.DATABASE_URL)
+    .then(() => {
+      console.log("Mongodb Database Connected");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+module.exports = databaseConnect;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+
+const databaseConnect = require("./config/database");
+
+dotenv.config({
+  path: "backend/config/config.env",
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("This is the backend!");
 });
-
+databaseConnect();
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
