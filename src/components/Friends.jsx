613b6fd59065b8a24d7a5ed6a08f731b39f1f5f1
Home PC <vfxprojectguy@gmail.com>
2024-03-28 16:10:00 -0700

    building messenger page

---
+++ src/components/Friends.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Friends = () => {
+  return (
+    <div className="friend">
+      <div className="friend-image">
+        <div className="image">
+          <img src="/image/29533marquee-08.jpg" alt="" />
+        </div>
+      </div>
+
+      <div className="friend-name-seen">
+        <div className="friend-name">
+          <h4>Friend</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Friends;
