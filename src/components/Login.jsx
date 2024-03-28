219a6e755c61e8394f9db6b3372a18f45bea3f60
Home PC <vfxprojectguy@gmail.com>
2024-03-27 22:52:36 -0700

    setup login authtoken

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { userLogin } from "../store/actions/authAction";
+
+import { useDispatch, useSelector } from "react-redux";
+import { useAlert } from "react-alert";
+import { ERROR_CLEAR, SUCCESS_MESSAGE_CLEAR } from "../store/types/authType";
+
 const Login = () => {
+  const navigate = useNavigate();
+
+  const alert = useAlert();
+  const { loading, authenticate, error, successMessage, myInfo } = useSelector(
+    (state) => state.auth
+  );
+
+  const dispatch = useDispatch();
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const inputHandle = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    dispatch(userLogin(state));
+  };
+
+  useEffect(() => {
+    if (authenticate) {
+      navigate("/");
+    }
+    if (successMessage) {
+      alert.success(successMessage);
+      dispatch({ type: SUCCESS_MESSAGE_CLEAR });
+    }
+    if (error) {
+      error.map((err) => alert.error(err));
+      dispatch({ type: ERROR_CLEAR });
+    }
+  }, [successMessage, error]);
+
   return (
     <div className="register">
       <div className="card">
@@ -8,11 +50,14 @@ const Login = () => {
           <h3>Login</h3>
         </div>
         <div className="card-body">
-          <form action="">
+          <form onSubmit={login}>
             <div className="form-group">
               <label htmlFor="email">Email</label>
               <input
                 type="email"
+                onChange={inputHandle}
+                name="email"
+                value={state.email}
                 className="form-control"
                 id="email"
                 placeholder="Email"
@@ -23,6 +68,9 @@ const Login = () => {
               <label htmlFor="password">Password</label>
               <input
                 type="password"
+                onChange={inputHandle}
+                name="password"
+                value={state.password}
                 className="form-control"
                 id="password"
                 placeholder="Password"
