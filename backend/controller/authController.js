3f0a06215906d05a812afdc508e2e78d0adc796c
Home PC <vfxprojectguy@gmail.com>
2024-03-29 22:30:12 -0700

    add messegerRoute and messengerController

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,6 +1,6 @@
 const formidable = require("formidable");
 const validator = require("validator");
-const registerModel = require("../models/authModels");
+const registerModel = require("../models/authModel");
 const fs = require("fs");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
