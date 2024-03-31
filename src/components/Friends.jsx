3a141df7a05bca5f792e1c0f4f3b03ed10203835
Home PC <vfxprojectguy@gmail.com>
2024-03-31 09:19:31 -0700

    fix bug on component Friends

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 const Friends = (friend) => {
-  console.log(friend);
+  console.log(friend.friend.image);
   return (
     <div className="friend">
       <div className="friend-image">
         <div className="image">
-          <img src={`./image/${friend.image}`} alt="" />
+          <img src={`./image/${friend.friend.image}`} alt="" />
         </div>
       </div>
 
       <div className="friend-name-seen">
         <div className="friend-name">
-          <h4>{friend.username}</h4>
+          <h4>{friend.friend.username}</h4>
         </div>
       </div>
     </div>
