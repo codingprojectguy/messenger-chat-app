ac1d8fd60080ab5220a8b8dcf253385e9f99979a
Home PC <vfxprojectguy@gmail.com>
2024-04-04 11:49:47 -0700

    update logout button function

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -198,3 +198,9 @@ module.exports.userLogin = async (req, res) => {
     }
   }
 };
+
+module.exports.userLogout = (req, res) => {
+  res.status(200).cookie("authToken", "").json({
+    success: true,
+  });
+};
