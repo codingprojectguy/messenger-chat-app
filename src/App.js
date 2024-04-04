f9cf0c0d9ac46743cdc0ed8d1362e5e6cf0770f5
Home PC <vfxprojectguy@gmail.com>
2024-04-04 11:32:46 -0700

    add protectRoute component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Messenger from "./components/Messenger";
-
+import ProtectRoute from "./components/ProtectRoute";
 function App() {
   return (
     <div className="App">
@@ -11,7 +11,15 @@ function App() {
         <Routes>
           <Route path="/messenger/login" element={<Login />} />
           <Route path="/messenger/register" element={<Register />} />
-          <Route path="/" element={<Messenger />} />
+          <Route
+            path="/"
+            element={
+              <ProtectRoute>
+                {" "}
+                <Messenger />{" "}
+              </ProtectRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
