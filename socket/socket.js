937ce3c472b62617e4200388a6ed1cf40e8c1b89
Home PC <vfxprojectguy@gmail.com>
2024-04-05 23:41:22 -0700

    update socket logout

--- socket/socket.js
+++ socket/socket.js
@@ -22,6 +22,10 @@ const findFriend = (id) => {
   return users.find((u) => u.userId === id);
 };
 
+const userLogout = (userId) => {
+  users = users.filter((u) => u.userId !== userId);
+};
+
 io.on("connection", (socket) => {
   console.log("Socket is connecting...");
   socket.on("addUser", (userId, userInfo) => {
@@ -66,7 +70,9 @@ io.on("connection", (socket) => {
       });
     }
   });
-
+  socket.on("logout", (userId) => {
+    userLogout(userId);
+  });
   socket.on("disconnect", () => {
     console.log("user is disconnect... ");
     userRemove(socket.id);
